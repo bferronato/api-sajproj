9237b549ac966127492bdd569b12ee636c8c60eb
Bruno <bferronato@gmail.com>
2017-06-11 16:06:24 -0300

    Create index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ con.query('SELECT * from cliente LIMIT 30', function(err, rows, fields) {
 	//con.end();
 	  if (!err) {
 	  	//response.send("The solution is: ", rows);
-		  res.status(status).send(body)
+		  response.status(status).send(body);
 	  	// var d = new Date();
 	  	console.log('The solution is: ', new Date());
 	  } else {
