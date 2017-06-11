22a07d6359128728319bf0d2aa36adec4cc4b29f
Bruno <bferronato@gmail.com>
2017-06-11 18:38:44 -0300

    Create server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 var express     = require('express');
 var app         = express();
 var bodyParser  = require('body-parser');
+/*
 var morgan      = require('morgan');
 var mongoose    = require('mongoose');
 
@@ -14,14 +15,16 @@ var User   = require('./app/models/user'); // get our mongoose model
 // =======================
 // configuration =========
 // =======================
+*/
 var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+/*
 mongoose.connect(config.database); // connect to database
 app.set('superSecret', config.secret); // secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+*/
 // use morgan to log requests to the console
 //app.use(morgan('dev'));
 
