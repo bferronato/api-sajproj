e8c073e158f9e4670f39b55dff96e27ddd7f499f
Bruno <bferronato@gmail.com>
2017-06-12 00:03:47 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secret':'ilovescotchyscotch',
-    'database':'mongodb://heroku_wtjsq288:Mongo2019@ds121222.mlab.com:21222/api-sajproj'
+    'database':'mongodb://heroku_wtjsq288:Mongo2019@ds121222.mlab.com:21222/heroku_wtjsq288'
 };
