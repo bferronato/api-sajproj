f3b1f56d9d1e2dc3b68e08ef3dcbf4d8f72b22ea
Bruno <bferronato@gmail.com>
2017-06-13 23:23:12 -0300

    Create config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secret':'ilovescotchyscotch',
-    'database':'mongodb://heroku_wtjsq288:Mongo2019@ds121222.mlab.com:21222/heroku_wtjsq288'
+    'database':'mongodb://api-sajproj:Mongo2019@ds121222.mlab.com:21222/heroku_wtjsq288'
 };
