25839babf383292a229dc7259c581610aa5d54f6
Bruno Henrique Ferronato <bferronato@gmail.com>
2020-05-10 02:51:50 -0300

    Script para geração de logins

--- index.js
+++ index.js
@@ -1,92 +1,124 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
-var mysql = require('mysql');
 var app = express();
+var login = require('./utils/login');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index')
+app.get('/', function (request, response) {
+  response.send('Hello World!');
 });
 
-app.get('/cool', function(request, response) {
+app.get('/:nome', function (request, response) {
+  var nome = request.params.nome;
+  response.send(login.geraLogin(nome))
+});
+
+app.get('/cool', function (request, response) {
   response.send(cool());
 });
 
-app.get('/nice', function(request, response) {
+app.get('/nice', function (request, response) {
   response.send("Nice 2x");
 });
 
-var con = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASS,
-  database : process.env.MYSQLBASE
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-app.get("/cliente", function(request, response){
-    con.query('SELECT * from cliente LIMIT 30', function(err, rows, fields) {
-	//con.end();
-	  if (!err) {
-	  	//response.send("The solution is: ", rows);
-		  response.status(200).json(rows); //.send({result:"The solution is: "});
-		  //res.json(rows);
-	  	// var d = new Date();
-	  	console.log('The solution is: ', new Date());
-	  } else {
-		  response.status(500);
-	  	console.log('Error while performing Query.');
-	  }
-    });
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 
-// var con = mysql.createPool({
-//   connectionLimit:50,
+
+// var cool = require('cool-ascii-faces');
+// var express = require('express');
+// var mysql = require('mysql');
+// var app = express();
+
+// app.set('port', (process.env.PORT || 5000));
+
+// app.use(express.static(__dirname + '/public'));
+
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+
+// app.get('/', function(request, response) {
+//   response.render('pages/index')
+// });
+
+// app.get('/cool', function(request, response) {
+//   response.send(cool());
+// });
+
+// app.get('/nice', function(request, response) {
+//   response.send("Nice 2x");
+// });
+
+// //"url": "https://github.com/heroku/node-js-getting-started"
+
+// var con = mysql.createConnection({
 //   host: process.env.MYSQLHOST,
 //   user: process.env.MYSQLUSER,
 //   password: process.env.MYSQLPASS,
 //   database : process.env.MYSQLBASE
 // });
 
-// app.get("/cliente", function(request, response){
-// 	con.getConnection(function(error, tempCont) {
-// 		if(!!error) {
-// 			tempCont.release();
-// 			console.log('Erro de conexao');
-// 		} else {
-// 			console.log('Conectado!');
-
-// 			tempCont.query('SELECT * from cliente LIMIT 30', function(err, rows, fields) {
-// 			});
-// 		}
-
-// 	});
-// con.query('SELECT * from cliente LIMIT 30', function(err, rows, fields) {
-// 	//con.end();
-// 	  if (!err) {
-// 	  	response.send("The solution is: ", rows);
-// 	  	// var d = new Date();
-// 	  	console.log('The solution is: ', new Date());
-// 	  } else {
-// 	  	console.log('Error while performing Query.');
-// 	  }
-//   });
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
 // });
 
+// // app.get("/cliente", function(request, response){
+// // con.query('SELECT * from cliente LIMIT 30', function(err, rows, fields) {
+// // 	//con.end();
+// // 	  if (!err) {
+// // 	  	response.send("The solution is: ", rows);
+// // 	  	// var d = new Date();
+// // 	  	console.log('The solution is: ', new Date());
+// // 	  } else {
+// // 	  	console.log('Error while performing Query.');
+// // 	  }
+// //   });
+// // });
 
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// // var con = mysql.createPool({
+// //   connectionLimit:50,
+// //   host: process.env.MYSQLHOST,
+// //   user: process.env.MYSQLUSER,
+// //   password: process.env.MYSQLPASS,
+// //   database : process.env.MYSQLBASE
+// // });
+
+
+
+// // app.get("/cliente", function(request, response){
+// // 	con.getConnection(function(error, tempCont) {
+// // 		if(!!error) {
+// // 			tempCont.release();
+// // 			console.log('Erro de conexao');
+
+// // 		} else {
+// // 			console.log('Conectado!');
+
+// // 			tempCont.query('SELECT * from cliente LIMIT 30', function(err, rows, fields) {
+// // 			});
+// // 		}
+
+// // 	});
+// // con.query('SELECT * from cliente LIMIT 30', function(err, rows, fields) {
+// // 	//con.end();
+// // 	  if (!err) {
+// // 	  	response.send("The solution is: ", rows);
+// // 	  	// var d = new Date();
+// // 	  	console.log('The solution is: ', new Date());
+// // 	  } else {
+// // 	  	console.log('Error while performing Query.');
+// // 	  }
+// //   });
+// // });
+
+
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
---
+++ utils/login.js
@@ -0,0 +1,79 @@
+/**
+ * Regras:
+ * 
+ * Caracteres com acentos devem ser substituítos por seus respectivos, por exemplo ã => a, ç => c. í => i e assim sucessivamente.
+ * Não pode possuir espacos em branco no inicio ou no final do login
+ * Não pode possuir aspas simples ou duplas
+ * Não pode possuir qualificadores "filho", "junior", "neto", "segundo", "terceiro", "sobrinho"
+ * Não pode possuir separadores  "da", "de", "di", "do", "du", "dos", "das"
+ * 
+ * O login deve se a combinacao de cada nome/sobrenome e/ou a inicial do nome/sobrenome em todas as combinacoes possiveis
+ * concatenados com TRACO ou UNDERLINE
+ * 
+ * Não pode possuir espaços em branco
+ * Login gerado deve possuir no maximo 20 caracteres
+ */
+const login = {
+    geraLogin(nome) {
+
+        const qualificadores = ["filho", "junior", "neto", "segundo", "terceiro", "sobrinho"];
+        const separadores = ["da", "de", "di", "do", "du", "dos", "das"];
+
+        nome = nome.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+        nome = nome.trim().toLowerCase()
+        nome = nome.replace('\'', '').replace('"', '');
+
+        let nomesSemQualificadores = nome.split(/\s+/).filter((nomeParte) => {
+            return !qualificadores.includes(nomeParte);
+        })
+
+        let nomesSemSeparadores = nomesSemQualificadores.filter((nomeParte) => {
+            return !separadores.includes(nomeParte);
+        })
+
+        var logins = this.getCombinations(nomesSemSeparadores);
+
+
+        logins = logins.filter(function (value, index, self) {
+            return self.indexOf(value) === index;
+        });
+
+        logins = logins.filter((login) => {
+            return login.length <= 20
+        })
+        
+        logins.unshift('Teste: João da Silva Gonçalves Sant\'Ana Junior<hr>')
+
+        result = logins.join('<br>')
+
+        return result
+    },
+    getCombinations(nomes) {
+        var result = [];
+        var f = function (prefix, nomes) {
+            for (var i = 0; i < nomes.length; i++) {
+                if (prefix) {
+                    result.push(prefix + '_' + nomes[i]);
+                    result.push(prefix + '_' + nomes[i][0]);
+                    result.push(prefix + '-' + nomes[i][0]);
+                    result.push(prefix[0] + '_' + nomes[i]);
+                    result.push(prefix[0] + '-' + nomes[i]);
+                }
+                if (prefix) {
+                    f(prefix + '_' + nomes[i], nomes.slice(i + 1));
+                    f(prefix + '-' + nomes[i], nomes.slice(i + 1));
+                } else {
+                    // Controla a geracao de logins somente para o primeiro nome
+                    if (i == 1) {
+                        return
+                    }
+                    f(prefix + nomes[i], nomes.slice(i + 1));
+                }
+            }
+        }
+        f('', nomes);
+        return result;
+    }
+}
+
+module.exports = login
